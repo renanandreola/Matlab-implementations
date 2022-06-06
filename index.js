f4bb36bf87eb624e7d518644d1c862db971f2822
Renan Andreolla <renanandreolla@outlook.com>
2022-06-06 00:38:57 -0300

    Histogram image

--- index.js
+++ index.js
@@ -4,6 +4,8 @@ var imageData1;
 var imageData2;
 var result;
 
+// cria Canvas e pega os elementos para montar o preview
+
 document.getElementById('arquivo1').onchange = function (evt) {
     var tgt = evt.target || window.event.srcElement,
         files = tgt.files;
@@ -34,7 +36,7 @@ function showImage2(fileReader) {
     img.src = fileReader.result;
 }
 
-// CREATE PREVIEW
+// CREATE PREVIEW - ACIMA
 
 // Ajustar casos de overflow > 255
 const adjustOverFlow = img => img.map(pixel => pixel > 255? pixel % 255: pixel);
@@ -70,6 +72,7 @@ function showImageResult(img) {
 // ajustar casos de pixel menor que 0 - underflow
 const adjustUnderFlow = img => img.map(pixel => pixel < 0? 0: pixel);
 
+// inicialização de operações
 var add = false;
 var subt = false;
 var mult = false;
@@ -79,7 +82,10 @@ var blend = false;
 var andd = false;
 var orr = false;
 var xorr = false;
-var nott = false;
+var nott1 = false;
+var nott2 = false;
+var calcHistImg1 = false;
+var calcHistImg2 = false;
 
 // operação que vem do front, variaveis criadas acima
 function operation(op) {
@@ -92,7 +98,10 @@ function operation(op) {
     op == 'and' ? andd = true : andd = false;
     op == 'or' ? orr = true : orr = false;
     op == 'xor' ? xorr = true : xorr = false;
-    op == 'not' ? nott = true : nott = false;
+    op == 'not1' ? nott1 = true : nott1 = false;
+    op == 'not2' ? nott2 = true : nott2 = false;
+    op == 'calcHistImg1' ? calcHistImg1 = true : calcHistImg1 = false;
+    op == 'calcHistImg2' ? calcHistImg2 = true : calcHistImg2 = false;
 }
 
 // enviar imagem para calcular resultado
@@ -148,9 +157,26 @@ async function sendImage() {
         showImageResult(resultXOR);
     }
 
-    if (nott) {
+    if (nott1) {
         const resultNOT = not(archive1,archive2);
         showImageResult(resultNOT);
+    } 
+    
+    if (nott2) {
+        const resultNOT = not2(archive1,archive2);
+        showImageResult(resultNOT);
+    }
+
+    if (calcHistImg1) {
+        calculateHistImg1(archive1,archive2);
+        // const resultHist1 = calculateHistImg1(archive1,archive2);
+        // showImageResult(resultHist1);
+    }
+
+    if (calcHistImg2) {
+        calculateHistImg2(archive1,archive2);
+        // const resultHist1 = calculateHistImg1(archive1,archive2);
+        // showImageResult(resultHist1);
     }
 }
 
@@ -199,10 +225,6 @@ function blending(firstMatriz, secondMatriz) {
       result[i] = parseFloat(num) * firstMatriz[i] + (1 - parseFloat(num)) * secondMatriz[i];
     }
 
-    // for (let j = 0; j < result.length; j++) {
-    //     result[j] = result[j] * parseFloat(num);
-    // }
-
     return adjustOverFlow(result);
 }
 
@@ -237,6 +259,7 @@ function divide(firstMatriz, secondMatriz) {
     }
 
     var num = $("#div").val();
+
     for (let j = 0; j < result.length; j++) {
         result[j] = result[j] * parseFloat(num);
     }
@@ -285,12 +308,162 @@ function xor(firstMatriz, secondMatriz) {
     return adjustOverFlow(result);
 }
 
-// realiza o NOT
+// realiza o NOT 1
 function not(firstMatriz, secondMatriz) {
     const result = [];
     for (let i = 0; i < firstMatriz.length; i++) {
       result[i] = firstMatriz[i] != secondMatriz[i];
+      result[i] = 255 - firstMatriz[i];
     }
 
     return adjustOverFlow(result);
 }
+
+// realiza o NOT 2
+function not2(firstMatriz, secondMatriz) {
+    const result = [];
+    for (let i = 0; i < firstMatriz.length; i++) {
+      result[i] = firstMatriz[i] != secondMatriz[i];
+      result[i] = 255 - secondMatriz[i];
+    }
+
+    return adjustOverFlow(result);
+}
+
+// calcular histograma da imagem 1
+function calculateHistImg1(firstMatriz, secondMatriz) {
+    // var pixels = [...Array(255)].map(()=> 0 );
+    
+    // console.log(firstMatriz.length);
+    
+    // Cria um array de contagem
+    const result1 = [];
+    // const array_contador = [];
+    var value = 0;
+
+    // Array para armazenar as labels do gráfico
+    const labels1 = [];
+
+    // adiciona 256 elementos ao array, todos valendo o número 0
+    for (let i = 0; i < 256; i++) {
+        result1.push(0);
+        labels1.push(i.toString());
+    }
+
+    //Percorre cada pixel da imagem. Pula de 4 em 4, pois percorre o RGBA
+    for (let j = 0; j < firstMatriz.length; j += 4) {
+        // Como a imagem é em escala de cinza, os valores de R, G e B são os mesmos
+        value = firstMatriz[j];
+
+        // Aumenta o contador de números repetidos nesse índice
+        result1[value]++;
+    }
+    
+    console.log(result1);
+
+    makeGraphic1(result1, labels1);
+}
+
+function makeGraphic1(result, labels) {
+    console.log(labels);
+    console.log(result);
+
+    const config = {
+        type: 'bar',
+        data: data = {
+            labels: labels,
+            datasets: [{
+              label: 'Quantidade de pixeis',
+              data: result,
+              backgroundColor: [
+                'rgb(156, 70, 71)',
+              ],
+              borderColor: [
+                'rgb(156, 70, 71)',
+              ],
+              borderWidth: 1
+            }]
+          },
+        options: {
+          scales: {
+            y: {
+              beginAtZero: false
+            }
+          }
+        },
+      };
+    image1 = new Chart(
+        document.getElementById('grafico-histograma1'),
+        config
+    );
+    $(".title-hist1").text('Histograma Imagem 1')
+}
+
+// calcular histograma da imagem 2
+// function calculateHistImg2(firstMatriz, secondMatriz) {
+//     // var pixels = [...Array(255)].map(()=> 0 );
+    
+//     // console.log(firstMatriz.length);
+    
+//     // Cria um array de contagem
+//     const result2 = [];
+//     // const array_contador = [];
+//     var value = 0;
+
+//     // Array para armazenar as labels do gráfico
+//     const labels2 = [];
+
+//     // adiciona 256 elementos ao array, todos valendo o número 0
+//     for (let i = 0; i < 256; i++) {
+//         result2.push(0);
+//         labels2.push(i.toString());
+//     }
+
+//     //Percorre cada pixel da imagem. Pula de 4 em 4, pois percorre o RGBA
+//     for (let j = 0; j < firstMatriz.length; j += 4) {
+//         // Como a imagem é em escala de cinza, os valores de R, G e B são os mesmos
+//         value = firstMatriz[j];
+
+//         // Aumenta o contador de números repetidos nesse índice
+//         result2[value]++;
+//     }
+    
+//     console.log(result2);
+
+//     makeGraphic2(result2, labels2);
+// }
+
+// function makeGraphic2(result, labels) {
+//     console.log(labels);
+//     console.log(result);
+
+//     const config = {
+//         type: 'bar',
+//         data: data = {
+//             labels: labels,
+//             datasets: [{
+//               label: 'Quantidade de pixeis',
+//               data: result,
+//               backgroundColor: [
+//                 'rgb(156, 70, 71)',
+//               ],
+//               borderColor: [
+//                 'rgb(156, 70, 71)',
+//               ],
+//               borderWidth: 1
+//             }]
+//           },
+//         options: {
+//           scales: {
+//             y: {
+//               beginAtZero: false
+//             }
+//           }
+//         },
+//       };
+//     image2 = new Chart(
+//         document.getElementById('grafico-histograma2'),
+//         config
+//     );
+//     $(".title-hist2").text('Histograma Imagem 2');
+// }
